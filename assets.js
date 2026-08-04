8638d9e9a112b4d76e1b883462d6f9292fa35c80
shukry sharby <shukrysharby91@gmail.com>
2026-08-04 10:27:37 +0800

    Load the complete cake image gallery

--- assets.js
+++ assets.js
@@ -1,6 +1,6 @@
 (() => {
   "use strict";
-  ["asset1.js", "asset2.js", "asset6.js"].forEach((src) => {
+  ["asset1.js", "asset2.js", "asset3.js", "asset4.js", "asset5.js", "asset6.js"].forEach((src) => {
     document.write(`<script src="${src}"><\/script>`);
   });
 })();
